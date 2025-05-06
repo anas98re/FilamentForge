f172d909219d4e9b601e8aa709fe802b4de533f1
anas <o@gamil.com>
2025-05-06 17:17:18 +0300

    Set Up Localization

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'greeting' => 'Hello dear,',
+    'invitation' => 'This is an invitation to participate in an emotional intelligence BCA36 survey.',
+    'survey_link' => 'Survey link: :link',
+    'contact_note' => 'Note: If you need any help, please contact us by email at support@email.com.',
+    'closing' => 'Regards, IT Team',
+    'invitation_subject' => 'Invitation to Participate in BCA36 Survey',
+    'send_invitation' => 'Send Invitation',
+    'no_invitations' => 'There is no invitation yet.',
+    'success_message' => 'Invitation sent successfully.',
+    'enter_email' => 'Please enter email here',
+    'cancel' => 'Cancel',
+];
